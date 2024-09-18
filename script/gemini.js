0e5f7e30c3e32b30915277391ca1712fcec9b63a
Puyit321 <jkingpuyit@gmail.com>
2024-09-18 23:12:40 +0800

    Update gemini.js

--- script/gemini.js
+++ script/gemini.js
@@ -1,36 +1,33 @@
 module.exports.config = {
-	name: "gemini",
-	role: 0,
-	credits: "Deku", //https://facebook.com/joshg101
-	description: "Talk to Gemini (conversational)",
-	hasPrefix: false,
-	version: "5.6.7",
-	aliases: ["bard"],
-	usage: "gemini [prompt]"
+  name: "gemini",
+  role: 0,
+  credits: "Deku", //Modiffied by churchill
+  description: "can describe picture",
+  hasPrefix: false,
+  version: "5.6.7",
+  aliases: ["bard"],
+  usage: "gemini [picture-url]"
 };
 
 module.exports.run = async function ({ api, event, args }) {
-	const axios = require("axios");
-	let prompt = args.join(" "),
-		uid = event.senderID,
-		url;
-	if (!prompt) return api.sendMessage(`Please enter a prompt.`, event.threadID);
-	api.sendTypingIndicator(event.threadID);
-	try {
-		const geminiApi = `https://deku-rest-api.gleeze.com/`;
-		if (event.type == "message_reply") {
-			if (event.messageReply.attachments[0]?.type == "photo") {
-				url = encodeURIComponent(event.messageReply.attachments[0].url);
-				const res = (await axios.get(`${geminiApi}/gemini?prompt=${prompt}&url=${url}`)).data;
-				return api.sendMessage(res.gemini, event.threadID);
-			} else {
-				return api.sendMessage('Please reply to an image.', event.threadID);
-			}
-		}
-		const response = (await axios.get(`${geminiApi}/gemini?prompt=${prompt}`)).data;
-		return api.sendMessage(response.gemini, event.threadID);
-	} catch (error) {
-		console.error(error);
-		return api.sendMessage('❌ | An error occurred. You can try typing your query again or resending it. There might be an issue with the server that\'s causing the problem, and it might resolve on retrying.', event.threadID);
-	}
+  const axios = require("axios");
+  let uid = event.senderID,
+    url;
+  if (event.type == "message_reply") {
+    if (event.messageReply.attachments[0]?.type == "photo") {
+      url = encodeURIComponent(event.messageReply.attachments[0].url);
+      api.sendTypingIndicator(event.threadID);
+      try {
+        const response = (await axios.get(`https://deku-rest-api.replit.app/gemini?prompt=describe%20this%20photo&url=${url}&uid=${uid}`)).data;
+        return api.sendMessage(response.gemini, event.threadID);
+      } catch (error) {
+        console.error(error);
+        return api.sendMessage('❌ | An error occurred. You can try typing your query again or resending it. There might be an issue with the server that\'s causing the problem, and it might resolve on retrying.', event.threadID);
+      }
+    } else {
+      return api.sendMessage('Please reply to an image.', event.threadID);
+    }
+  } else {
+    return api.sendMessage(`Please enter a picture URL or reply to an image with "gemini answer this".`, event.threadID);
+  }
 };
