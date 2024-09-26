6053b5736b9a97c71ee6d0d827836b1c2c8045e0
Puyit321 <jkingpuyit@gmail.com>
2024-09-27 00:12:58 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -28,12 +28,12 @@ module.exports.run = async function ({ api, event, args }) {
         // Delay
         await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust the delay time as needed
 
-        const gpt4_api = `https://cprojectapisjonellv2.adaptable.app/api/chatgpt?input=${encodeURIComponent(prompt)}`;
+        const gpt4_api = `https://nash-rest-api-production.up.railway.app/gpt-3.5_turbo?prompt=${encodeURIComponent(prompt)}`;
 
         const response = await axios.get(gpt4_api);
 
-        if (response.data && response.data.result) {
-            const generatedText = response.data.result;
+        if (response.data && response.data.reply) {
+            const generatedText = response.data.reply;
 
             // Ai Answer Here
             api.sendMessage(`•| 𝙱𝙾𝙶𝙰𝚁𝚃 𝙰𝙸 𝙱𝙾𝚃 |• ━━━━━━━━━━━━━━━━\n${generatedText}\n━━━━━━━━━━━━━━━━\n•| 𝙾𝚆𝙽𝙴𝚁 : 𝙷𝙾𝙼𝙴𝚁 𝚁𝙴𝙱𝙰𝚃𝙸𝚂 |•`, event.threadID, messageID);
