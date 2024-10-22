e7afce86e0fb7e9e909129f51bd696bb7de03e0b
kyleigop089 <wuukayshin@gmail.com>
2024-10-22 13:14:51 +0800

    Update and rename ai.js to ai2.js

--- script/ai2.js
+++ script/ai2.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports.config = {
-    name: "ai",
+    name: "ai2",
     version: "1.0.0",
     hasPermission: 0,
     credits: "kylepogi",//api by jerome
